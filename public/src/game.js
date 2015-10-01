bab068c3eed120a5d02d29150545263daa7e8bde
pawank94 <pawanknitkkr@gmail.com>
2015-10-01 18:42:48 +0530

    level 1 complete, complete collision layer , spike killing added, enemy added with functionality, trophy added

--- public/src/game.js
+++ public/src/game.js
@@ -1,72 +1,152 @@
+var counter=0;
 var Q = Quintus()
-	.include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio')
+	.include('Sprites, Scenes, Input, 2D, Anim, Touch, UI')
 	.setup({
-	maximize: true
+		maximize: true
 	}).controls().touch();
 var objectFiles = [
-	'./src/player'];
+	'./src/objects'];
 require(objectFiles, function() {
 	Q.scene('arena', function(stage) {
+		/*
+		*  collision layers
+		*/
 		stage.collisionLayer(new Q.TileLayer({
-			tileW: 35,  // Default tile width
- 		 	tileH: 35,  // Default tile height
-			blockTileW: 35,  // Default pre-render size
+			tileW: 35,
+ 		 	tileH: 35,
+			blockTileW: 35,
+			blockTileH: 35,
+			dataAsset: '/maps/bg.json',
+			sheet: 'bg',
+			type: 0
+		}));
+		stage.collisionLayer(new Q.TileLayer({
+			tileW: 35,
+ 		 	tileH: 35,
+			blockTileW: 35,
 			blockTileH: 35,
 			dataAsset: '/maps/simple_block.json',
 			sheet: 'simple_block'
 		}));
 		stage.collisionLayer(new Q.TileLayer({
-			tileW: 35,  // Default tile width
- 		 	tileH: 35,  // Default tile height
-			blockTileW: 35,  // Default pre-render size
+			tileW: 35,
+ 		 	tileH: 35,
+			blockTileW: 35,
 			blockTileH: 35,
 			dataAsset: '/maps/ground_grass.json',
 			sheet: 'grass'
 		}));
 		stage.collisionLayer(new Q.TileLayer({
-			tileW: 35,  // Default tile width
- 		 	tileH: 35,  // Default tile height
-			blockTileW: 35,  // Default pre-render size
+			tileW: 35,
+ 		 	tileH: 35,
+			blockTileW: 35,
 			blockTileH: 35,
 			dataAsset: '/maps/water.json',
 			sheet: 'water',
 			type: 0
 		}));
 		stage.collisionLayer(new Q.TileLayer({
-			tileW: 35,  // Default tile width
- 		 	tileH: 35,  // Default tile height
-			blockTileW: 35,  // Default pre-render size
+			tileW: 35,
+ 		 	tileH: 18,
+ 		 	dataAsset: '/maps/spikes.json',
+ 		 	sheet:'spike'
+		}));
+		stage.collisionLayer(new Q.TileLayer({
+			tileW: 35,
+ 		 	tileH: 35,
+			blockTileW: 35,
 			blockTileH: 35,
 			dataAsset: '/maps/box.json',
 			sheet: 'box'
 		}));
 		stage.collisionLayer(new Q.TileLayer({
-			tileW: 35,  // Default tile width
- 		 	tileH: 35,  // Default tile height
-			blockTileW: 35,  // Default pre-render size
+			tileW: 35,
+ 		 	tileH: 35,
+			blockTileW: 35,
+			blockTileH: 35,
+			dataAsset: '/maps/water_varient.json',
+			sheet: 'water_varient',
+			type: 0
+		}));
+		stage.collisionLayer(new Q.TileLayer({
+			tileW: 35,
+ 		 	tileH: 35,
+			blockTileW: 35,
 			blockTileH: 35,
 			dataAsset: '/maps/pipe.json',
 			sheet: 'pipe'
 		}));
 		stage.collisionLayer(new Q.TileLayer({
-			tileW: 35,  // Default tile width
- 		 	tileH: 35,  // Default tile height
-			blockTileW: 35,  // Default pre-render size
+			tileW: 35,
+ 		 	tileH: 35,
+			blockTileW: 35,
 			blockTileH: 35,
 			dataAsset: '/maps/pipe_head.json',
 			sheet: 'pipe_head'
 		}));
-		/*stage.collisionLayer(new Q.TileLayer({
-			tileW: 70, 
-  			tileH: 70,
-			dataAsset: "/maps/simple_block.json",
-			sheet: "simple_block"
-		}));*/
+		/////////////////objects////////////////////
+
 		var player = stage.insert(new Q.Player({
 			x: 700,
 			y: 50
 		}));
-		stage.add('viewport').follow(player,{x:false,y:false});
+		var trophy = stage.insert(new Q.Trophy({
+			x:1190,
+			y:560,
+			scale:1.1
+		}));
+		populateEnemy();//enemy population
+		/////////////////////////////////////////////
+		stage.add('viewport').follow(player,{x:true,y:false},{scale:1.5});//.centerOn(Q.width/2-40,Q.height/2-20);
+		/*
+		* Enemy populate
+		*/
+		function populateEnemy()
+		{
+			var a=0,b=0;
+			var si=setInterval(function(){
+				console.log('here');
+				var enemy = stage.insert(new Q.Enemy({
+					x: 680+a,
+					y: 300-b
+				}));
+				if(a==200)
+					stop_insert();
+				a+=200;
+				b=50;
+			}, 100);
+			function stop_insert(){
+				clearInterval(si);
+			};
+			for(var i=0;i<3;i++)
+			{
+				var enemy = stage.insert(new Q.Enemy({
+					x: 1000+i*10,
+					y: 500
+				}));
+			}
+		}
+		///////////////////////////////////////
+	});
+	/*
+	*	death scene
+	*/
+	Q.scene('dead',function(stage){
+		var container = stage.insert(new Q.UI.Container({
+			x: Q.width/2, y: Q.height/2, fill: "rgb(0,0,0)"
+		}));
+		var label = container.insert(new Q.UI.Text({x:10, y: -10, color:"rgb(0,151,123)" ,label: "You are dead" }));
+		container.fit(20);
+	});
+	/*
+	*	winning scene
+	*/
+	Q.scene('win',function(stage){
+		var container = stage.insert(new Q.UI.Container({
+			x: Q.width/2, y: Q.height/2, fill: "rgb(0,0,0)"
+		}));
+		var label = container.insert(new Q.UI.Text({x:10, y: -10, color:"rgb(0,151,123)" ,label: "You Won!!!" }));
+		container.fit(20);
 	});
 	var files = [
 		'/images/tiles.png',
@@ -79,14 +159,34 @@ require(objectFiles, function() {
 		'/maps/pipe.json',
 		'/maps/pipe_head.json',
 		'/maps/water.json',
+		'/maps/spikes.json',
+		'/maps/bg.json',
+		'/maps/water_varient.json',
 		'/images/sprites.png',
+		'/images/trophy.png',
+		'/images/spike.png',
+		'/images/spk.png',
 		'/maps/ground_grass.json',
-		'/images/sprites.json',
-		'/images/background.png'];
+		'/images/sprites.json'];
+	/*
+	* load resources
+	*/
 	Q.load(files.join(','), function() {
 		Q.sheet('golplayer','/images/player.png',{
 			tilew: 35,
 			tileh: 50
+		});
+		Q.sheet('trophy','/images/trophy.png',{
+			tilew: 40,
+			tileh: 45
+		});
+		Q.sheet('spike','/images/spk.png',{
+			sx: 46	,
+	        sy: 76,
+	        rows: 1,
+	        tilew: 30,
+	        tileh: 30,
+	        frames: 1
 		});
 		Q.compileSheets('/images/sprites.png', '/images/sprites.json');
 		Q.compileSheets('/images/tiles_map.png', '/images/tile.json');
---
+++ public/src/objects.js
@@ -0,0 +1,99 @@
+require([], function () {
+	Q.Sprite.extend('Player', {
+	init: function (p) {
+		this._super(p, {
+		jumpSpeed: -290,
+		speed: 200,
+		sheet: 'golplayer'
+	});
+	this.add('2d, platformerControls');
+	/*
+	* hit function
+	*/
+	this.on('hit',function(collision){
+		if(collision.obj.isA('Trophy'))
+		{
+			this.p.direction = "top";
+			Q.stageScene("win",2);
+			Q.stage().pause();
+			this.p.x+=40;
+			collision.obj.destroy();
+			counter=1;
+		}
+		if(collision.obj.p.sheet=='spike'&&!counter)
+		{
+			/*this.destroy();
+			Q.stageScene("dead",1);*/
+			this.p.x=700;
+			this.p.y=50;
+		}
+		if(collision.obj.p.sheet=='water_varient'&&!counter)
+		{
+			collision.obj.p.type = 0;
+			collision.obj.destroy();
+		}
+
+	});
+	///////////////////////////////////
+	},
+	step: function (dt) {
+		if(Q.inputs['left'] && this.p.direction == 'right') {
+            this.p.flip = 'x';
+        }
+	    if(Q.inputs['right']  && this.p.direction == 'left') {
+	        this.p.flip = false;
+	    }
+	}
+	});
+	Q.Sprite.extend('Enemy' , {
+		init: function(p){
+			this._super(p,{
+				sheet: 'enemy',
+				vx: -100,
+				visiblityOnly: true,
+				scale:0.8
+			});
+			this.add('2d','aiBounce');
+			this.on('bump.left',function(collision){
+				if(collision.obj.isA('Player')&&!counter)
+				{
+					collision.obj.p.x=700;
+					collision.obj.p.y=50;
+					/*collision.obj.destroy();
+					Q.stageScene("dead",1);*/
+				}
+				else
+				{
+					this.p.vx=100;
+				}
+			});
+			this.on('bump.right',function(collision){
+				if(collision.obj.isA('Player')&&!counter)
+				{
+					/*collision.obj.destroy();
+					Q.stageScene("dead",1);*/
+					collision.obj.p.x=700;
+					collision.obj.p.y=50;
+				}
+				else
+				{
+					this.p.vx=-100;
+				}
+			});
+			this.on('bump.top',function(collision){
+				if(collision.obj.isA('Player'))
+				{
+					this.destroy();
+				}
+			});
+		}
+	 });
+	Q.Sprite.extend("Trophy", {
+			init: function(p){
+				console.log('here');
+				this._super(p,{
+					sheet:'trophy'
+				});
+			}
+		})
+});
